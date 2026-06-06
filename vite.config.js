c5aa9d3d5b5d8e0afa20d34b486457783c7a08bd
amcthonon <amcthonon@gmail.com>
2026-06-06 11:18:01 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,6 @@ export default defineConfig({
   base: '/',
   plugins: [react()],
   server: {
-    allowedHosts: [
-      "4fb9-24-206-110-82.ngrok-free.app"
-    ],
     port: 3000,
     open: true
   }
